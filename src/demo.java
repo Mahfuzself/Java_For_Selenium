d2f121ac38396d8dd0455d2e22fb7bd41c316788
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2023-10-05 12:34:31 +0600

    Some changes

--- src/demo.java
+++ src/demo.java
@@ -1,5 +1,10 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("Learning java for  Selenium");
+        for(int i = 0; i < 50 ; i++){
+            if(i%2==0){
+                System.out.println("Even number : ",+i);
+            }
+        }
     }
 }
