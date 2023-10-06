ff28620c75adec43fbd1b23c39c18b5f53ec6aff
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2023-10-06 09:41:49 +0600

    Even number print

---
+++ src/Class.java
@@ -0,0 +1,17 @@
+public class Class {
+    public static  class myClass{
+        int x;
+        int y;
+        public  myClass(){
+            x = 8;
+            y = 9;
+        }
+        public int sum(){
+            return x+y;
+        }
+    }
+    public static void main(String[] args) {
+        myClass myObj = new myClass();
+        System.out.println(myObj.sum());
+    }
+}
--- src/demo.java
+++ src/demo.java
@@ -1,10 +1,18 @@
 public class demo {
+    public static class Print{
+       String name = "John" + " " + "Doe";
+    }
+
     public static void main(String[] args) {
         System.out.println("Learning java for  Selenium");
         for(int i = 0; i < 50 ; i++){
             if(i%2==0){
                 System.out.println("Even Number : "+i);
             }
+            else System.out.println("Odd Number : "+i);
         }
+        //class
+        Print myObj = new Print();
+        System.out.println(myObj.name);
     }
 }
